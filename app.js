86bae1d0de16760383f55fc11ac7dc40970a079e
Mark Miyashita <negativetwelve@gmail.com>
2013-07-13 01:38:42 -0700

    adding realtime data coming down from the server

--- app.js
+++ app.js
@@ -11,9 +11,19 @@ var express = require('express')
   , path = require('path');
 
 var app = express();
+var server = app.listen(3000);
+var io = require('socket.io').listen(server);
+
+var twitter = require('ntwitter');
+
+var twit = new twitter({
+  consumer_key: 'cu5fonPmIHE49ORYjCXUfw',
+  consumer_secret: 'ilzoOo60K7UKjkjapkAyQd1c21r9VuGDiSCzhTOdQ',
+  access_token_key: '83308529-t3GLeKnGJT2MFi9YPQDutkcli7m4NOszHKmeFgQWs',
+  access_token_secret: 'hptgcpkIbpTzqJRUeoEYF89HyKRXYPsfqXvqWogvLg'
+});
 
 // all environments
-app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(express.favicon());
@@ -34,5 +44,28 @@ app.get('/map', map.show);
 app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+  console.log('Express server listening on port 3000');
+});
+
+io.sockets.on('connection', function (socket) {
+  socket.on('get_tweets', function (data) {
+
+    twit.stream('statuses/sample', function(stream) {
+      stream.on('data', function (data) {
+        coordinates = data.coordinates;
+        if (coordinates) {
+          media = data.entities ? data.entities.media : null;
+          media_url = media ? media.map(function(item) { return item.media_url; }) : null;
+          socket.emit('tweet', {
+            media_url: media_url,
+            coordinates: coordinates,
+            username: data.user.screen_name,
+            time: data.created_at,
+            text: data.text
+          });
+        }
+      });
+    });
+
+  });
 });
