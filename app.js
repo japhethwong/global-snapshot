51530e665749dd263f831528f06b8d4f2c0fa0ef
Mark Miyashita <negativetwelve@gmail.com>
2013-07-13 14:01:50 -0700

    updating

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ var io = require('socket.io').listen(server);
 
 io.configure(function () { 
   io.set("transports", ["xhr-polling"]); 
-  io.set("polling duration", 10); 
+  io.set("polling duration", 30); 
 });
 
 var twitter = require('ntwitter');
